1baa3ce6901bcad7974d2756d2d95da588063305
Ankit <ankit2dbg@gmail.com>
2023-11-09 09:45:05 +0530

    latest commit

--- src/pages/UserDashboard.js
+++ src/pages/UserDashboard.js
@@ -5,6 +5,9 @@ import React,{ useEffect, useState } from 'react'
 import Link from 'next/link';
 import { useToasts } from 'react-toast-notifications';
 import LoadingSpinner from "../component/Loader";
+import * as moment from 'moment'
+import ReactDOMServer from "react-dom/server";
+import jsPDF from "jspdf";
 
 
 
@@ -50,6 +53,188 @@ export default function UserDashboard() {
         setUserDetails({})
         router.push('/')
       }
+
+    const downloadTicket = (item) => {
+        const html = `<html>
+        <head>
+            <meta charset="utf-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+            <title></title>
+            <style type="text/css">
+                p{
+                    margin: 0px !important;
+                }
+                .container {
+                    width:800px;
+                }
+            </style>
+        </head>
+        <body>
+        <div>
+            <div class="container" style="border:1px solid #000">
+                <div class="row">
+                    <div class="col-sm-2">
+                        <img src="img/download.jpg" class="img-fluid" style="height: 125px;">
+                    </div>
+                    <div class="col-sm-10">
+                        <p class="text-end">TRAVELS D K</p>
+                        <p class="text-end">S/O : RAMESH CHANDRA, ASAINAPUR, ASAINA, KANPUR. DEHAT ASIANAPUR,</p>
+                        <p class="text-end">UTTAR PRADESH-209301,</p>
+                        <p class="text-end">Kanpur,</p>
+                        <p class="text-end">INDIA, Phone : 9818877541</p>
+                        <p class="text-end">Email Id : traveldk286@gmail.com</p>
+                    </div>
+        
+                </div>
+        <div class="row" style="border-top:1px solid #000;border-bottom:1px solid #000">
+        
+                <div class="col-sm-6">
+                <p  class="text-start">Your e-Ticket</p>
+                </div>
+                
+                <div class="col-sm-6"><p class="text-end">3-Sep-2023</p></div>
+            </div>
+            <div class="row" style="border-top:1px solid #000;border-bottom:1px solid #000">
+        
+                <div class="col-sm-6">
+                    <img src="img/1.png" height="25px">
+                <p  class="text-start">Air Asia-I5 775</p>
+                </div>
+                
+                <div class="col-sm-6">
+                    <h3 class="text-end">Booking Reference : E3512J</h3>
+                <h3 class="text-end">Ticket Status : Booked</h3></div>
+            </div>
+            <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Departure </th>
+              <th scope="col">Arrival</th>
+              <th scope="col">Flight No </th>
+              <th scope="col">Class </th>
+              <th scope="col">Pnr</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>
+                  <p>Guwahati[GAU]</p>
+        <p>04/09/2023 22:45</p></td>
+              <td><p>Delhi[DEL]</p>
+                  <p>5/09/2023 01:25 </p></td>
+              <td>I5-775</td>
+              <td>BT </td>
+              <td>E3512J</td>
+            </tr>
+            
+          </tbody>
+        </table>
+        <h4>Pax Details Customer Contact No:6900286627</h4>
+            <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Passenger Name</th>
+              <th scope="col">Ticket No</th>
+              <th scope="col">Baggage </th>
+              <th scope="col">Meal</th>
+              <th scope="col">Seat </th>
+              <th scope="col">Refundable  </th>
+              <th scope="col">Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Ms NIMA ZANGMU(Adult) </td>
+              <td>3512J </td>
+              <td>15 KG </td>
+              <td>BT </td>
+              <td>NA</td>
+              <td>Yes</td>
+              <td>Booked</td>
+            </tr>
+            <tr>
+              <td>Ms NIMA ZANGMU(Adult) </td>
+              <td>3512J </td>
+              <td>15 KG </td>
+              <td>BT </td>
+              <td>NA</td>
+              <td>Yes</td>
+              <td>Booked</td>
+            </tr>
+            <tr>
+              <td>Ms NIMA ZANGMU(Adult) </td>
+              <td>3512J </td>
+              <td>15 KG </td>
+              <td>BT </td>
+              <td>NA</td>
+              <td>Yes</td>
+              <td>Booked</td>
+            </tr>
+            
+          </tbody>
+        </table>
+        
+        
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">Basic Fare</th>
+              <th scope="col">Tax & Others </th>
+              <th scope="col">ross Amount</th>
+              
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>INR 16616 </td>
+              <td>INR 8804</td>
+              <td>NR 25420</td>
+              
+            </tr>
+           
+            
+          </tbody>
+        </table>
+        
+        
+        <h5>Term And Condition</h5>
+        <ul>
+            <li>All passengers must produce a valid photo identification proof at the time of check in.</li>
+            <li> Guests will be subjected to a security screening prior to boarding the aircraft. This is mandatory per Indian
+        Regulations.</li>
+            <li> For Flights within Indian Check-in counters open 3 hours prior to departure of flights and close 1 hour prior to
+        departure of flights. You may be denied boarding if you do not report in time.
+        </li>
+            <li>For International flights Check-in usually starts 3 hours prior to departure, and Check- in counters will be closed
+        75 minutes before departure time for all classes of guests.
+        </li>
+            <li>Flight timings are subject to change without prior notice. Please recheck with the carrier prior to departure</li>
+            <li>For Fare Rules/ Cancellation policty-refer to fare rules laid by the carrier.</li>
+            <li> While compliling the all above information, we have endeavored to ensure that all information is correct.
+        However, no guarantee or representation is made to the accuracy or completenses of the information contained
+        here. This information is cubject to changes by airlines without notice.
+        </li>
+            <li>For bookints made under class HBO fare, each of the passenger shall be allowed to carry only one(01) piece of
+        luggage of maximum seven(07) kg. of weight (having maximum overall dimensions of:115 cms. (L+W+H) on
+        Boeigng aircarft and 108 cms(L+W+H) on Bombardier aircarft).</li>
+        </ul>
+        
+        
+        
+        
+        </div>
+            
+        </div>
+        </body>
+        </html>`
+        const doc = new jsPDF("p", "pt", "letter");
+        doc.html(html, {
+        callback: function (doc) {
+            doc.save('sample.pdf');
+        }
+        });
+    }
 
     return (
         <>
@@ -189,18 +374,20 @@ export default function UserDashboard() {
                                                             <th>Booking type</th>
                                                             <th>Booking amount</th>
                                                             <th>Status</th>
-                                                            {/* <th>Action</th> */}
+                                                            <th>Date of Booking</th>
+                                                            <th>Download Ticket</th>
                                                         </tr>
                                                     </thead>
                                                     <tbody>
                                                     {myBookings.map((item, i) => (
                                                         <tr>
                                                             <td>{i+1}</td>
-                                                            <td>#{item.booking_id}</td>
+                                                            <td>{JSON.parse(item.booking_response).BookingId}</td>
                                                             <td>Flight</td>
                                                             <td>{'Rs. '+item.total_price}</td>
                                                             <td class="complete">Completed</td>
-                                                            {/* <td><i class="fas fa-eye"></i></td> */}
+                                                            <td>{moment(new Date(item.created_at)).format('DD-MM-YYYY h:mm a')}</td>
+                                                            <td><a href='javascript:void(0)' onClick={(e) => downloadTicket(item)}><i class="fas fa-download"></i></a></td>
                                                         </tr>
                                                     ))}
                                                         {/* <tr>
